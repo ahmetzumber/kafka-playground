a2ed2e680892cb5179611f6dc30756b1d7f04070
ahmetzumber <ahmetzumber5@gmail.com>
2022-09-18 15:09:09 +0300

    event producing handled and constants added.

---
+++ producer/producer.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+	"github.com/ahmetzumber/kafka-demo/utils"
+	"github.com/segmentio/kafka-go"
+	"time"
+)
+
+func main() {
+	conn, _ := kafka.DialLeader(context.Background(), utils.NETWORK, utils.ADDRESS, utils.TOPIC_NAME, utils.PARTITION)
+	conn.SetWriteDeadline(time.Now().Add(time.Second * 10))
+
+	conn.WriteMessages(kafka.Message{ Value: []byte("catch the first event !!") })
+}
+
---
+++ utils/constants.go
@@ -0,0 +1,8 @@
+package utils
+
+var (
+	NETWORK    = "tcp"
+	ADDRESS    = "localhost:9092"
+	TOPIC_NAME = "demo_topic"
+	PARTITION  = 0
+)
